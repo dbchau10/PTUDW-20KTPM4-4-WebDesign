4a415e476d143cb0fdccfb1deebab3c8f3d33292
dbchau10 <82007790+dbchau10@users.noreply.github.com>
2022-11-28 00:40:13 +0700

    responsive update

--- main.js
+++ main.js
@@ -4,15 +4,20 @@ owl.owlCarousel({
     margin:10,
     dots: false,
     nav:true,
-    autoplay:true,
-    autoplayTimeout:1000,
-    autoplayHoverPause:true,
+    center: true,
+    // autoplay:true,
+    // autoplayTimeout:1000,
+    // autoplayHoverPause:true,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:2
+        },
+
+        780: {
+            items: 3
         },
         1000:{
             items:4

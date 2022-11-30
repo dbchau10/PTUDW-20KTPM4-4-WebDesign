68ea92b518e71b5cb11f3d1108aa0007b3fe4a85
dbchau10 <cdangbao@ploggmedia.com>
2022-11-30 19:24:38 +0700

    token update

--- password.js
+++ password.js
@@ -1,3 +1,4 @@
+let otpInput = document.querySelector(".digit-group .input");
 
 
     
@@ -11,3 +12,28 @@
         digit.style.display="inline-block";
 
     }
+
+    $('.digit-group').find('input').each(function() {
+        $(this).attr('maxlength', 1);
+        $(this).on('keyup', function(e) {
+            console.log($(this).parent());
+            var parent = $($(this).parent());
+            console.log(parent);
+            if(e.keyCode === 8 || e.keyCode === 37) {
+                var prev = parent.find('input#' + $(this).data('previous'));
+                
+                if(prev.length) {
+                    $(prev).select();
+                }
+            } else if((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode >= 96 && e.keyCode <= 105) || e.keyCode === 39) {
+                var next = parent.find('input#' + $(this).data('next'));
+                
+                if(next.length) {
+                    $(next).select();
+                } else {
+                    $('.btn-token').trigger('click');
+                }
+            }
+        });
+    });
+

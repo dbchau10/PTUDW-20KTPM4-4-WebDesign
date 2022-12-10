aa61d8c171e96c6217e10a68facc91c68b9a9426
nhat2012782 <gonbbb1@gmail.com>
2022-12-11 02:11:45 +0700

    update sort, filter, pop up modal

--- src/SearchPage/main.js
+++ src/SearchPage/main.js
@@ -28,20 +28,75 @@
 //     }
 
 let offcanvas = document.querySelector(".navbar-toggler");
-  offcanvas.addEventListener('click', canvasHandling);
+offcanvas.addEventListener("click", canvasHandling);
+const $$ = document.querySelectorAll.bind(document);
+const $ = document.querySelector.bind(document);
 
-  function canvasHandling(e){
-    e.preventDefault();
-    let $nav = $("nav");
-    $nav.removeClass('navbar-light bg-light shadow').addClass('navbar-dark');
+function canvasHandling(e) {
+  e.preventDefault();
+  let $nav = $("nav");
+  $nav.removeClass("navbar-light bg-light shadow").addClass("navbar-dark");
+}
 
-  }
+let close = document.querySelector(".btn-close");
+close.addEventListener("click", closeHandler);
 
-  let close = document.querySelector(".btn-close");
-  close.addEventListener('click', closeHandler);
+function closeHandler(e) {
+  e.preventDefault();
+  let $nav = $("nav");
+  $nav.removeClass("navbar-dark").addClass("navbar-light bg-light shadow");
+}
 
-  function closeHandler(e){
-    e.preventDefault();
-    let $nav = $("nav");
-    $nav.removeClass('navbar-dark').addClass('navbar-light bg-light shadow');
-    }
+function handleModalPayMent() {
+  const navItems = $$(".main__nav-item");
+  const mainContainers = $$(".main__container");
+  const mainFooters = $$(".main__footer");
+  const closeIcon = $(".modal__close-icon");
+  const modal = $(".modal");
+
+  Array.from(navItems).forEach((navItem, index) => {
+    navItem.onclick = () => {
+      const navItemActive = $(".main__nav-item.active");
+      const mainContainerActive = $(".main__container.active");
+      const mainFooterActive = $(".main__footer.active");
+
+      navItemActive.classList.remove("active");
+      mainContainerActive.classList.remove("active");
+      mainFooterActive.classList.remove("active");
+
+      navItems[index].classList.add("active");
+      mainContainers[index].classList.add("active");
+      mainFooters[index].classList.add("active");
+    };
+  });
+
+  const backTexts = $$(".back-text");
+  Array.from(backTexts).forEach((backText, index) => {
+    backText.onclick = () => {
+      index;
+      const navItemActive = $(".main__nav-item.active");
+      const mainContainerActive = $(".main__container.active");
+      const mainFooterActive = $(".main__footer.active");
+
+      navItemActive.classList.remove("active");
+      mainContainerActive.classList.remove("active");
+      mainFooterActive.classList.remove("active");
+
+      navItems[index].classList.add("active");
+      mainContainers[index].classList.add("active");
+      mainFooters[index].classList.add("active");
+    };
+  });
+
+  closeIcon.onclick = () => {
+    modal.style.display = "none";
+  };
+}
+
+function handleChooseBtn() {
+  const modal = $(".modal");
+  modal.style.display = "block";
+}
+
+handleModalPayMent();
+handleChooseBtn();

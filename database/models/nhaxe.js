b2f1937efe7428f671def8feca8035ef12273bb7
vdkhoi20 <vdkhoi.itai@gmail.com>
2022-12-28 22:21:58 +0700

    model nha xe

---
+++ database/models/nhaxe.js
@@ -0,0 +1,39 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class nhaxe extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      nhaxe.hasMany(models.chuyenxe,{foreignKey:'idnhaxe'}); 
+      nhaxe.belongsToMany(models.khachhang,{through:'danhgias',
+                                foreignKey:'idnhaxe'});
+    }
+  }
+  nhaxe.init({
+    tennhaxe: DataTypes.STRING,
+    noidunggioithieu: DataTypes.TEXT,
+    anhnhaxe: DataTypes.STRING,
+    sdt: {
+        type:DataTypes.STRING,
+        unique: true
+    },
+    diachi: DataTypes.STRING,
+    email: {
+        type:DataTypes.STRING,
+        unique: true
+    },
+    website: DataTypes.STRING,
+    soSaoTB: DataTypes.FLOAT
+  }, {
+    sequelize,
+    modelName: 'nhaxe',
+  });
+  return nhaxe;
+};

696b59b53148463ddcd70e4e41770d199934fa82
hmttai <josephho382@gmail.com>
2023-01-09 19:55:23 +0700

    update link layout

--- database/routes/accountmanageRoute.js
+++ database/routes/accountmanageRoute.js
@@ -1,6 +1,10 @@
 const express=require("express");
 const router=express.Router();
 
+const path = require("path");
+router.use(express.static(path.join(__dirname, '..', '/static')))
+
+
 router.use('/', require('./loginRoute'))
 router.use('/', require('./signUpRoute'))
 router.use('/', require('./signUpDetailsRoute'))

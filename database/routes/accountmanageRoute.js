d0771767377b97cee51dcf944d870827bf2016ec
nhat2012782 <gonbbb1@gmail.com>
2023-01-09 20:23:32 +0700

    fix title

--- database/routes/accountmanageRoute.js
+++ database/routes/accountmanageRoute.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const path = require("path");
 router.use(express.static(path.join(__dirname, "..", "/static")));
 
-router.use("/signUpRoute", require("./signUpRoute"));
-router.use("/signUpDetailsRoute", require("./signUpDetailsRoute"));
-router.use("/forgetPasswordRoute", require("./forgetPasswordRoute"));
+router.use("/signUp", require("./signUpRoute"));
+router.use("/signUpDetails", require("./signUpDetailsRoute"));
+router.use("/forgetPassword", require("./forgetPasswordRoute"));
 router.use("/", require("./loginRoute"));
 
 module.exports = router;
